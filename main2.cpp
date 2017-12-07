f63555a9fcdbcbd762ad7cf0cedb61237d8a4667
Unknown <sodiqagbaje@gmail.com>
2017-12-07 12:57:09 -0500

    Worked on similarity in main

--- main2.cpp
+++ main2.cpp
@@ -8,11 +8,17 @@ int main(int argc, char* argv){
   if(argc < 2){
     return 1; exit(EXIT_FAILURE);
   }
+
+double similar;
 for (int i = 1; i < argc - 1; i++){
   languages lang1 =  languages(agrv[i]);
   languages test = languages(argv[-1]);
   double comparision = setSimilarity(lang1, test);
-
+  if (comparision[i] < comparision[i+1] && similar < comparision[i]){
+    similar = comparision[i];}
+  else{
+    continue;}
 }
+  std::cout<<similar
   return 0;
 }
