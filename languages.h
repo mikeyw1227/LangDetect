7d4031313c762e47f3b2f6096f82cfd1083720b9
Michael White <mikeyw1227@my.uri.edu>
2017-12-07 12:29:37 -0500

    Added a set name function

--- languages.h
+++ languages.h
@@ -48,6 +48,9 @@ public:
   //returns the similarity between two vectors as a double
   double setSimilarity(std::vector<int> triFreq1, std::vector<int> triFreq2);
 
+  //funciton to set the name of a triFreq
+  void setName(std::string name;)
+
 
   //Getter Functions
   std::vector<int> getTrigramFreq();
