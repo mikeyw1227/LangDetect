a9f908da5dddd0343855c236c8a787aff9e0840e
Michael White <mikeyw1227@my.uri.edu>
2017-12-07 12:40:47 -0500

    fixed white space and added some comments

--- main2.cpp
+++ main2.cpp
@@ -8,6 +8,13 @@ int main(int argc, char* argv){
   if(argc < 2){
     return 1; exit(EXIT_FAILURE);
   }
-
+  //use if statement to do for the lowest comparision
+  //get name for each language
+  //return the name of a language
+  for (int i = 1; i < argc - 1; i++){
+    languages lang1 =  languages(agrv[i]);
+    languages test = languages(argv[-1]);
+    double comparision = setSimilarity(lang1, test);
+  }
   return 0;
 }
