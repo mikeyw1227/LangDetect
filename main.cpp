091d809890a2b7f8462edfaf5ca1d3e05af2ebff
Unknown <sodiqagbaje@gmail.com>
2017-12-10 14:53:22 -0500

    Fixed (well attempted to fix) the bad input case

--- main.cpp
+++ main.cpp
@@ -8,12 +8,21 @@ int main(int argc, char *argv[]){
   if(argc <= 2){
     return 1; exit(EXIT_FAILURE);
   }
+
 double comp = 0;
 std::string name;
 languages similar;
+for (int i = 1; i < argc; i++){
+  std::ifstream my_file(argv[i]);
+if (my_file.good()){continue;}
+else{return 1; exit(EXIT_FAILURE);}
+}
+
 for (int i = 1; i < argc - 2; i++){
   //language to be tested
+
   languages lang = languages(argv[i]);
+
   lang.setName(argv[i]);
   //setting the languages trigramfreq
   std::vector<int> langtf = lang.getTrigramFreq();
